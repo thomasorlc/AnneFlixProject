09cace7c8eda94794bea2e65a10b95af4572193c
Antoine DERIAN <a.derian@net.estia.fr>
2021-12-12 15:07:13 +0100

    V1.1 Databinding mis en place

--- app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.gmail.eamosse.imdb.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,39 +8,44 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.NavHostFragment
 import com.gmail.eamosse.imdb.R
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.gmail.eamosse.imdb.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
-    private val homeViewModel: HomeViewModel by viewModel()
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.token.observe(viewLifecycleOwner, Observer {
-            textView.text = "${it.requestToken} - ${it.expiresAt}"
-        })
-
-        homeViewModel.error.observe(viewLifecycleOwner, Observer {
-            textView.text = "Erreur $it"
-        })
-        return root
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<View>(R.id.button_home).setOnClickListener {
+        homeViewModel.token.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = "${it.requestToken} - ${it.expiresAt}"
+        })
+
+        homeViewModel.error.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = "Erreur $it"
+        })
+
+        binding.buttonHome.setOnClickListener {
             val action = HomeFragmentDirections
-                    .actionHomeFragmentToHomeSecondFragment("From HomeFragment")
+                .actionHomeFragmentToHomeSecondFragment("From HomeFragment")
             NavHostFragment.findNavController(this@HomeFragment)
-                    .navigate(action)
+                .navigate(action)
         }
     }
 }
